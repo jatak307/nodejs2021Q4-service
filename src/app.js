8a201e222f5e3c0d47f4b54c7bd30726ec047d5f
jatak307 <jatak307@gmail.com>
2021-12-02 00:30:05 +0300

    feat: add errorHundler module

--- src/app.js
+++ src/app.js
@@ -1,15 +1,13 @@
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
 
+const { errorHundler } = require('./common/error');
 const UserRoutes = require('./resources/users/user.router');
  
 const app = new Koa();
 app.use(bodyParser());
 
-// app.use(async ctx => {
-//   ctx.body = ctx.request.body;
-// });
-// console.log(UserRoutes);
+app.use(errorHundler);
 
 app.use(UserRoutes.routes())
   .use(UserRoutes.allowedMethods());
---
+++ src/common/error.js
@@ -0,0 +1,14 @@
+const errorHundler = async (ctx, next) => {
+  try {
+    await next();
+  } catch (err) {
+    ctx.status = err.statusCode || err.status || 500;
+    ctx.body = {
+        message: err.message
+    };
+  }
+};
+
+module.exports = {
+  errorHundler
+}
