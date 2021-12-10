4950c36ab8be654f1b9212b079fc371f25d21b07
jatak307 <jatak307@gmail.com>
2021-12-10 18:03:48 +0300

    feat: add the typing of board memory repository module

--- src/resources/boards/board.memory.repository.ts
+++ src/resources/boards/board.memory.repository.ts
@@ -3,23 +3,28 @@ import { CreateBoard } from './board.models';
 
 const boardsData: Map<string, Board> = new Map();
 
-const getBoards = (): Board[] => [...boardsData.values()];
+function getBoards(): Board[] {
+  return [...boardsData.values()];
+}
 
-const getOneBoard = (id: string) => boardsData.get(id);
+function getOneBoard(id: string): Board | undefined {
+  return boardsData.get(id);
+}
 
-const createNewBoard = (obj: CreateBoard) => {
+function createNewBoard(obj: CreateBoard): Board {
   const board = new Board(obj);
   boardsData.set(board.id, board);
   return board;
-};
+}
 
-const updateBoardById = (id: string, body: CreateBoard) => {
+function updateBoardById(id: string, body: CreateBoard): Board {
   let updatedBoard = boardsData.get(id);
   updatedBoard = { id, ...body };
   boardsData.set(id, updatedBoard);
   return updatedBoard;
-};
+}
 
-const deleteBoardById = (id: string) => boardsData.delete(id);
-
+function deleteBoardById(id: string): void {
+  boardsData.delete(id);
+}
 export { getBoards, getOneBoard, createNewBoard, updateBoardById, deleteBoardById };
