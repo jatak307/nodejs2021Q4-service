feabc747fe712cdc26df9883e0b46d449821fe1f
jatak307 <jatak307@gmail.com>
2022-01-16 18:28:19 +0300

    feat: add connection data for DB to environment and config

--- src/common/config.ts
+++ src/common/config.ts
@@ -8,10 +8,14 @@ dotenv.config({
 interface Config {
   PORT: number,
   NODE_ENV: string | undefined,
-  MONGO_CONNECTION_STRING: string | undefined,
   JWT_SECRET_KEY: string | undefined,
   AUTH_MODE: boolean,
-  LOG_LEVEL: string
+  LOG_LEVEL: string,
+  POSTGRES_PORT: number,
+  POSTGRES_USER: string,
+  POSTGRES_PASSWORD: string,
+  POSTGRES_DB: string,
+  POSTGRES_HOST: string,
 }
 
 enum LoggingLevels {
@@ -23,10 +27,14 @@ enum LoggingLevels {
 const config: Config = {
   PORT: +(process.env.PORT || 4000),
   NODE_ENV: process.env.NODE_ENV,
-  MONGO_CONNECTION_STRING: process.env.MONGO_CONNECTION_STRING,
   JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
   AUTH_MODE: process.env.AUTH_MODE === 'true',
   LOG_LEVEL: process.env.LOG_LEVEL || LoggingLevels[2],
+  POSTGRES_PORT: +(process.env.POSTGRES_PORT || 5432),
+  POSTGRES_USER: process.env.POSTGRES_USER || 'postgres',
+  POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD || 'postgres',
+  POSTGRES_DB: process.env.POSTGRES_DB || 'postgres',
+  POSTGRES_HOST: process.env.POSTGRES_HOST || 'postgres',
 };
 
 export { 
--- src/common/ormconfig.ts
+++ src/common/ormconfig.ts
@@ -2,16 +2,16 @@ import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConne
 import { Board } from '../entity/board.model';
 import { Columns } from '../entity/column.model';
 import { Task } from '../entity/task.model';
-
 import { User } from '../entity/user.model';
+import { config } from './config';
 
 const typeOrmConfig: PostgresConnectionOptions = {
-  type: "postgres",
-  host: "postgres",
-  port: 5432,
-  username: "postgres",
-  password: "postgres",
-  database: "postgres",
+  type: 'postgres',
+  host: config.POSTGRES_HOST,
+  port: config.POSTGRES_PORT,
+  username: config.POSTGRES_USER,
+  password: config.POSTGRES_PASSWORD,
+  database: config.POSTGRES_DB,
   synchronize: true,
   logging: false,
   entities: [
