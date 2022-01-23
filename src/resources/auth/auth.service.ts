7a5db8b80770b1e920379b6fcf500248b34a2edd
jatak307 <jatak307@gmail.com>
2022-01-23 21:19:28 +0300

    feat: write logic for function basicAuth

--- src/resources/auth/auth.service.ts
+++ src/resources/auth/auth.service.ts
@@ -1,10 +1,39 @@
+import jwt from 'jsonwebtoken';
 import { Context } from "koa";
+import { config } from '../../common/config';
+import { User } from "../../entity/user.model";
 
-const basicAuth = (ctx: Context) => {
+const basicAuth = async (ctx: Context) => {
+  const JWT_SECRET_KEY = { config };
   try {
-    console.log('success');
-  } catch (error) {
-    console.log(error);
+    const { password } = ctx.request.body
+    if (!ctx.request.body.login && !password) {
+      ctx.body = 'Who you are?';
+      ctx.status = 401;
+    }
+
+    const userTarget = await User.findOne({ name: ctx.request.body.login })
+    if (userTarget) {
+      const { id, login } = userTarget;
+      const jwtToken = jwt.sign(
+        {
+          id,
+          login
+        },
+        JWT_SECRET_KEY as unknown as string
+      );
+      ctx.status = 201;
+      ctx.body = {
+        token: jwtToken
+      }
+    } else {
+      ctx.status = 403;
+      ctx.body = "User not Found";
+    }
+
+  }
+  catch (err) {
+    console.log(err);
   }
 };
 
