373aa17a0c7776cacb3ba8b82c9d612d8d6c5db6
jatak307 <jatak307@gmail.com>
2022-02-04 23:39:03 +0300

    init: START Task 10. Nest.js. Feat: install nest and set up the app

--- src/resources/auth/auth.router.ts
+++
@@ -1,8 +0,0 @@
-import Router from "@koa/router";
-import { basicAuth } from "./auth.service";
-
-export const routerLogin = new Router;
-
-routerLogin.post('/login', async (ctx) => {
-  await basicAuth(ctx);
-});
