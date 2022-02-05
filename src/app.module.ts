243832504d250feffa139ddf6243c15f70221f1b
jatak307 <jatak307@gmail.com>
2022-02-05 20:45:59 +0300

    feat: create structure of app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ResourcesModule } from './resources/resources.module';
 
 @Module({
-  imports: [],
+  imports: [ResourcesModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
